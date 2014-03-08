c533d2a1cdbe350d911ddb23a8fafe1fd3f5b80f
Alexander Chernyakhovsky <achernya@mit.edu>
2014-03-08 01:43:57 -0500

    Release 1.4

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='moira-gui',
+      version='1.4',
+      description='Graphical utility for editing the Moira database',
+      author='Debathena Project',
+      scripts=['xmoira']
+      )
